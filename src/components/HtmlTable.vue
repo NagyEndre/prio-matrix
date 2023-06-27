ffcdc2e43d8bfe7e239e920209573d5a8abbf45b
Endre Nagy <endretamasnagy@gmail.com>
2023-06-27 19:18:25 +0200

    FEAT: Add prio table skeleton

---
+++ src/components/HtmlTable.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+import PrioCell from "./PrioCell.vue";
+
+const msg = ref("Hello World!");
+const prios = ["HIGH", "MID", "LOW"];
+
+function getClass(prio) {
+  return prio.toLowerCase();
+}
+</script>
+
+<template>
+  <table>
+    <tr>
+      <th class="space-cell"></th>
+      <th class="space-cell"></th>
+      <th colspan="3" class="title">Urgency</th>
+    </tr>
+    <tr>
+      <th class="space-cell"></th>
+      <th class="space-cell"></th>
+      <th v-for="prio in prios" :class="getClass(prio)" class="col-width">
+        {{ prio }}
+      </th>
+    </tr>
+
+    <tr>
+      <th rowspan="4" class="title">Impact</th>
+    </tr>
+    <tr v-for="prio in prios">
+      <th :class="getClass(prio)">{{ prio }}</th>
+      <td v-for="index in 3">
+        <prio-cell></prio-cell>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<style>
+th,
+td {
+  border: 1px solid grey;
+  border-radius: 0.2rem;
+}
+.title {
+  background-color: cornflowerblue;
+}
+.high {
+  background: red;
+}
+.mid {
+  background: yellow;
+}
+.low {
+  background: green;
+}
+.col-width {
+  min-width: 10rem;
+}
+.space-cell {
+  border: none;
+}
+</style>
---
+++ src/components/PrioCell.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="prio-cell">
+    <input
+      v-show="isInputShown"
+      v-model="newTask"
+      ref="taskInput"
+      @keyup.enter="onInput(newTask)"
+      placeholder="Add task"
+    />
+    <ul>
+      <li v-for="item in items">
+        <span @click="onSelection(item)">{{ item }}</span>
+        <button @click="deleteItem(item)">remove</button>
+      </li>
+    </ul>
+    <div class="right-alignment">
+      <button @click="showInput">+</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const items = ref([]);
+const taskInput = ref(null);
+const isInputShown = ref(false);
+const newTask = ref("");
+
+function showInput() {
+  isInputShown.value = true;
+  taskInput.value.focus();
+}
+
+function deleteItem(item) {
+  console.log(`deleting item ${item}`);
+  const i = items.value.indexOf(item);
+  items.value.splice(i, 1);
+}
+
+function onSelection(item) {
+  console.log(`selected item ${item}`);
+}
+
+function onInput(input) {
+  items.value.push(input);
+  isInputShown.value = false;
+  newTask.value = "";
+}
+</script>
+
+<style>
+.right-alignment {
+  text-align: right;
+}
+button {
+  margin-left: 1rem;
+}
+ul {
+  list-style-type: none;
+}
+.prio-cell {
+  height: 100%;
+}
+</style>
